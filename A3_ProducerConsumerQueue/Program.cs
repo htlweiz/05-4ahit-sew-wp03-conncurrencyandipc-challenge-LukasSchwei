7fafde7b37d361aa7edc5513028a6227d5c69318
LukasSchwei <schwei220080@sr.htlweiz.at>
2025-11-10 09:13:08 +0100

    done number 3

--- A3_ProducerConsumerQueue/Program.cs
+++ A3_ProducerConsumerQueue/Program.cs
@@ -12,21 +12,47 @@ class Program
         Console.WriteLine("Übung 3: Producer-Consumer");
         Console.WriteLine("==========================================\n");
 
-       // TODO
-       
+        Producer[] producers = new Producer[5];
+        ConcurrentQueue<int> queue = new ConcurrentQueue<int>();
+        for (int i = 0; i < 5; i++)
+        {
+            Producer p = new Producer(i, queue);
+            producers[i] = p;
+        }
+
 
         Console.WriteLine("Producer und Consumer gestartet...\n");
 
         // Überwachung: Jede Sekunde Queue-Füllstand ausgeben und auf >50 prüfen
-        
+
         // TODO
 
+        Thread checker = new Thread(() => CheckQueue(queue, producers));
+        checker.Start();
+        checker.Join();
+
+        foreach (Producer p in producers)
+        {
+            p.Stop();
+        }
+        Console.WriteLine($"Final Count: {queue.Count()}");
 
         // Alle Producer stoppen
-       
+
 
         // Consumer stoppen
-       
-       
+
+
+    }
+    
+    static void CheckQueue(ConcurrentQueue<int> queue, Producer[] producers)
+    {
+        while(true)
+        {
+            if (queue.Count() >= 50)
+            {
+                return;
+            }
+        }
     }
 }
