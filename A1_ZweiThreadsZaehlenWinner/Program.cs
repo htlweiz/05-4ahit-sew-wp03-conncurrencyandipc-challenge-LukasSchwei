2c9cc6800fe515dfed3398c1849fe5c3d1691d6c
LukasSchwei <schwei220080@sr.htlweiz.at>
2025-11-10 08:01:13 +0100

    commit 1 finish

--- A1_ZweiThreadsZaehlenWinner/Program.cs
+++ A1_ZweiThreadsZaehlenWinner/Program.cs
@@ -9,18 +9,25 @@ class Program
     
     public static void Main(string[] args)
     {
-        Console.WriteLine("Übung 1: Zwei Threads – Zählen & Winner");
-        
-        
+        new Thread(CountUpThreadA).Start();
+        new Thread(CountDownThreadB).Start();
     }
     
     private static void CountUpThreadA()
     {
-        
+        for (int i = 1; i <= 100; i++)
+        {
+            Console.WriteLine($"Thread 1: {i}");
+            Thread.Sleep(100);
+        }
     }
     
     private static void CountDownThreadB()
     {
-       
+       for (int i = 100; i >= 1; i--)
+        {
+            Console.WriteLine($"Thread 2: {i}");
+            Thread.Sleep(100);
+        }
     }
 }
