26951d15fb918363a21a3306b2b0ffa479092205
LukasSchwei <schwei220080@sr.htlweiz.at>
2025-11-10 08:40:13 +0100

    done with 2

--- A2_RaceConditionBank/Program.cs
+++ A2_RaceConditionBank/Program.cs
@@ -13,12 +13,36 @@ class Program
         // Bankkonto mit Startwert 1000 EUR erstellen
         BankAccount account = new BankAccount(1000);
         Console.WriteLine($"Startkontostand: {account.GetBalance()} EUR\n");
+
+        Thread[] threads = new Thread[10];
+
+        for (int i = 0; i < 10; i++)
+        {
+            Thread thread = new Thread(() => PerformBankOperations(account));
+            threads[i] = thread;
+            thread.Start();
+        }
+
+        foreach (Thread thread in threads)
+        {
+            thread.Join();
+        }
         
+        Console.WriteLine($"Endbetrag: {account.GetBalance()}");
     }
     
     private static void PerformBankOperations(BankAccount account)
     {
-        
+        object lockObject = new object();
+        lock (lockObject)
+        {
+            account.Deposit(100);
+        }
+        Thread.Sleep(500);
+        lock (lockObject)
+        {
+            account.Withdraw(150);
+        }
     }
 }
 
